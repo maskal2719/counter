622e9d58e6bf5b219277e25b2a6fc116e9050a10
Stanislav Moskalenko <maskal2719@gmail.com>
2023-03-02 10:21:04 +0300

    add error on incorrect value

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, RefObject, useRef, useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import './App.css';
 import {Display} from './components/Display/Display';
 import {SuperButton} from "./components/SuperButton/SuperButton";
@@ -6,13 +6,17 @@ import {SuperButton} from "./components/SuperButton/SuperButton";
 
 function App() {
 
-
+    const errMessage = 'Error'
 
     const [count, setCount] = useState<number>(0)
-    const [min, setMinCount] = useState<number>(0)
+    const [minCount, setMinCountCount] = useState<number>(0)
     const [maxCount, setMaxCount] = useState<number>(5)
-    const isDisableIcn = count >= maxCount
-    const isDisableReset = count === min
+    const [error, setError] = useState<boolean>(false)
+    const [disabledDisplay, setDisabledDisplay] = useState<boolean>(false)
+
+    const isDisableIcn = count >= maxCount || disabledDisplay
+    const isDisableReset = count === minCount || disabledDisplay
+    const isDisableSet = !disabledDisplay || error
 
 
     const inc = () => {
@@ -21,26 +25,34 @@ function App() {
         }
     }
     const reset = () => {
-        setCount(min)
+        setCount(minCount)
     }
 
     const setMinInputCount = (e: ChangeEvent<HTMLInputElement>) => {
-        setMinCount(+e.currentTarget.value)
+        const newMinCount = (+e.currentTarget.value)
+        setDisabledDisplay(true)
+        newMinCount >= maxCount ? setError(true) : setError(false)
+        setMinCountCount(+e.currentTarget.value)
     }
 
     const setMaxInputCount = (e: ChangeEvent<HTMLInputElement>) => {
+        const newMaxCount = (+e.currentTarget.value)
+        setDisabledDisplay(true)
+        newMaxCount <= minCount || newMaxCount <= 0 ? setError(true) : setError(false)
         setMaxCount(+e.currentTarget.value)
     }
 
+
     const onClickHandler = () => {
-        setCount(min)
+        setCount(minCount)
+        setDisabledDisplay(false)
     }
 
 
     return (
         <>
             <div className="container">
-                <Display count={count} maxCount={maxCount}/>
+                <Display count={count} maxCount={maxCount} disabledDisplay={disabledDisplay} error={error} errMessage={errMessage}/>
                 {/*{count >= 5 && <div className={'err'}>Error</div> }*/}
                 <div className={'btn-container'}>
                     <SuperButton name={'Inc'} callBack={inc} isDisable={isDisableIcn}/>
@@ -49,14 +61,15 @@ function App() {
             </div>
             <div className={'setting'}>
                 <h2>Settings</h2>
+                {error ? <div className={'err'}>{errMessage}</div> : ''}
                 <div>
-                    Start count:<input value={min} onChange={setMinInputCount} className={'input'} type="number"/>
+                    Start count:<input value={minCount} onChange={setMinInputCount}  className={`input ${error ? 'input_error' : ''}`} type="number"/>
                 </div>
                 <div>
-                    Max count:<input value={maxCount} onChange={setMaxInputCount} className={'input'} type="number"/>
+                    Max count:<input value={maxCount} onChange={setMaxInputCount} className={`input ${error ? 'input_error' : ''}`} type="number"/>
                 </div>
                 <div className={'btn-container'}>
-                    <SuperButton name={'Set'} callBack={onClickHandler} isDisable={false}/>
+                    <SuperButton name={'Set'} callBack={onClickHandler} isDisable={isDisableSet}/>
                 </div>
             </div>
         </>
--- src/components/Display/Display.tsx
+++ src/components/Display/Display.tsx
@@ -2,11 +2,16 @@ import React, {FC} from 'react';
 type DisplayPropsType = {
     count: number
     maxCount: number
+    disabledDisplay: boolean
+    error: boolean
+    errMessage: string
 }
-export const Display: FC<DisplayPropsType> = ({count, maxCount}) => {
+export const Display: FC<DisplayPropsType> = ({count, maxCount, disabledDisplay,error,errMessage}) => {
+
+    // const finalClassName = `${s.button} + ${xType === 'red' ? s.red : xType === 'secondary' ? s.secondary : s.default} ${disabled ? s.disabled : ''}`;
     return (
-        <div className={`display ${count == maxCount ? 'err' : ''}`}>
-            {count}
+        <div className={`display ${count === maxCount ? 'err' : ''}`}>
+            {disabledDisplay ? 'Press set value' : error ? 'errMessage' : count}
         </div>
     );
 };
